342a78be4ebf316b9fec87208ecb32c6393073ac
radmanlo <radman.lotfiazar@gmail.com>
2023-04-01 20:23:18 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+class Dog{
+
+    // constructor
+    constructor (name, sex){
+        this.name = name
+        this.sex = sex
+    }
+
+    // speak function
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+class Cat{
+
+    // constructor
+    constructor (name, sex){
+        this.name = name
+        this.sex = sex
+    }
+
+    // speak function
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+class Bird{
+
+    // constructor
+    constructor (name, sex){
+        this.name = name
+        this.sex = sex
+    }
+
+    // speak function
+    speak(){
+        if (this.sex === "male")
+            return `It\'s me! ${this.name}, the parrot!`
+        return `${this.name} says squawk!`
+    }
+}
